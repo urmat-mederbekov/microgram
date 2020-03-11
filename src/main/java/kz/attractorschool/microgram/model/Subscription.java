5ed2204de02290410fac5b3790ef71cf3469dd91
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-03-12 03:12:50 +0600

    filename changed

---
+++ src/main/java/kz/attractorschool/microgram/model/Subscription.java
@@ -0,0 +1,28 @@
+package kz.attractorschool.microgram.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Document(collection = "subscriptions")
+@Data
+//@CompoundIndex(def = "{'following': 1. 'followed': 1}")
+public class Subscription {
+    @Id
+    private String id;
+    private String following;
+    private String followed;
+    private LocalDateTime dateTime;
+
+    public Subscription(String following, String followed, LocalDateTime dateTime) {
+        this.id = UUID.randomUUID().toString();
+        this.following = following;
+        this.followed = followed;
+        this.dateTime = dateTime;
+    }
+
+}
