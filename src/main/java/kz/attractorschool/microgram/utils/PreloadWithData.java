bf2838fa746399325e39ed92d0f3b8b523bb56d6
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-03-12 03:19:56 +0600

    drastic changes made

--- src/main/java/kz/attractorschool/microgram/utils/PreloadWithData.java
+++ src/main/java/kz/attractorschool/microgram/utils/PreloadWithData.java
@@ -10,85 +10,58 @@ import java.util.stream.Stream;
 
 @Configuration
 public class PreloadWithData {
-    LocalDateTime dateTime = LocalDateTime.now();
-    @Bean
-    CommandLineRunner initUserDatabase(CommentRepository commentRep, LikedRepository likedRep,
-                                   PostRepository postRep, SubscriptionToUserRepository subscriptionToUserRep,
-                                   UserRepository userRep) {
-        userRep.deleteAll();
 
-        return  (args) -> Stream.of(makeUsers())
-                .peek(System.out::println)
-                .forEach(userRep::save);
-    }
     @Bean
-    CommandLineRunner initCommentDatabase(CommentRepository commentRep) {
-        commentRep.deleteAll();
+    CommandLineRunner initLikedDatabase(LikeRepository likeRep) {
+        likeRep.deleteAll();
 
-        return  (args) -> Stream.of(makeComments())
-                .peek(System.out::println)
-                .forEach(commentRep::save);
-    }
-    @Bean
-    CommandLineRunner initLikedDatabase(LikedRepository likedRep) {
-        likedRep.deleteAll();
-
-        return  (args) -> Stream.of(makeLikeds())
-                .peek(System.out::println)
-                .forEach(likedRep::save);
+        Post.getPosts().get(2).addLike(Like.getLikes().get(0));
+        Post.getPosts().get(1).addLike(Like.getLikes().get(2));
+        return (args) -> Stream.of(Like.getLikes())
+                .peek(likes -> likes.forEach(System.out::println))
+                .forEach(likeRep::saveAll);
     }
     @Bean
     CommandLineRunner initPostDatabase(PostRepository postRep) {
         postRep.deleteAll();
 
-        return  (args) -> Stream.of(makePosts())
-                .peek(System.out::println)
-                .forEach(postRep::save);
+        Post.getPosts().stream().forEach(post -> post.updateNumOfLikes());
+
+        return (args) -> Stream.of((Post.getPosts()))
+                .peek(posts -> posts.forEach(System.out::println))
+                .forEach(postRep::saveAll);
     }
+
     @Bean
-    CommandLineRunner initSubscriptionDatabase(SubscriptionToUserRepository subscriptionToUserRep) {
-        subscriptionToUserRep.deleteAll();
+    CommandLineRunner initUserDatabase(UserRepository userRep) {
+        userRep.deleteAll();
+        Post post = Post.getPosts().get(2);
+        User.getUsers().get(0).addPost(post);
+        Post post1 = Post.getPosts().get(1);
+        User.getUsers().get(0).addPost(post1);
 
-        return  (args) -> Stream.of(makeSubscriptionToUser())
-                .peek(System.out::println)
-                .forEach(subscriptionToUserRep::save);
+        User.getUsers().stream().forEach(user -> user.updateNumOfPosts());
+
+        return (args) -> Stream.of(User.getUsers())
+                .peek(users -> users.forEach(System.out::println))
+                .forEach(userRep::saveAll);
     }
 
-    private User[] makeUsers() {
-        return new User[]{
-                new User("1","thanos", "thanos@gmail.com", "thanos", 10, 6,324),
-                new User("2","loki", "loki@gmail.com", "loki", 150, 57,444),
-                new User("3","thor", "thor@gmail.com", "thor", 123, 565,546),
-                new User("4","batman", "batman@gmail.com", "batman", 20, 3567,656),
-                new User("5","superman", "superman@gmail.com", "superman", 40, 9085,633)
-        };
-    }
-    private Comment[] makeComments() {
-        return new Comment[]{
-                new Comment("1","thanos is god", dateTime),
-                new Comment("2","loki didn't die", dateTime.minusDays(4)),
-                new Comment("5","superman is lit", dateTime.minusMinutes(5))
-        };
-    }
-    private Post[] makePosts() {
-        return new Post[]{
-                new Post("1","", "Thanos is mad", dateTime),
-                new Post("2","", "Loki is murdered by Thanos", dateTime.minusDays(4)),
-                new Post("5","", "Superman sucks", dateTime.minusMinutes(5))
-        };
-    }
-    private SubscriptionToUser[] makeSubscriptionToUser() {
-        return new SubscriptionToUser[]{
-                new SubscriptionToUser("1", "Loki", "Thanos", dateTime),
-                new SubscriptionToUser("2","Superman", "Loki", dateTime.minusDays(4)),
-                new SubscriptionToUser("5","Thor", "Superman", dateTime.minusMinutes(5))
-        };
-    }
-    private Liked[] makeLikeds() {
-        return new Liked[]{
-                new Liked("1", "Loki", "Thanos", dateTime),
-                new Liked("2","Superman", "I can fly", dateTime.minusDays(4)),
-                new Liked("5","Thor", "loki is dead", dateTime.minusMinutes(5))
-        };
-    }
+    //    @Bean
+//    CommandLineRunner initCommentDatabase(CommentRepository commentRep) {
+//        commentRep.deleteAll();
+//
+//        return  (args) -> Stream.of(makeComments())
+//                .peek(System.out::println)
+//                .forEach(commentRep::save);
+//    }
+
+//    @Bean
+//    CommandLineRunner initSubscriptionDatabase(SubscriptionToUserRepository subscriptionToUserRep) {
+//        subscriptionToUserRep.deleteAll();
+//
+//        return  (args) -> Stream.of(makeSubscriptionToUser())
+//                .peek(System.out::println)
+//                .forEach(subscriptionToUserRep::save);
+//    }
 }
