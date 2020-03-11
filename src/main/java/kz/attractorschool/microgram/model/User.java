751345100340ce409780929264bf4aeebddb280a
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-03-12 03:21:03 +0600

    post amount updated is added

--- src/main/java/kz/attractorschool/microgram/model/User.java
+++ src/main/java/kz/attractorschool/microgram/model/User.java
@@ -14,42 +14,50 @@ import java.util.UUID;
 @Data
 //@CompoundIndex(def = "{'accountName': 1. 'email': 1}")
 public class User {
+    public static final User EMPTY = new User("No one", "none@gmail.com", "nothing",0,0);
 
     @Id
     private String id;
     private String accountName;
     private String email;
     private String password;
-    private int numOfPosts;
+    private int numOfPosts = 0;
     private int numOfFollowers;
     private int numOfFollowings;
     private static List<User> users = makeUsers();
+
     @DBRef
     private List<Post> posts = new LinkedList<>();
 
-    public User(String accountName, String email, String password, int numOfPosts, int numOfFollowers, int numOfFollowings) {
-        this.id =  UUID.randomUUID().toString();
+    public User(String accountName, String email, String password, int numOfFollowers, int numOfFollowings) {
+        this.id = UUID.randomUUID().toString();
         this.accountName = accountName;
         this.email = email;
         this.password = password;
-        this.numOfPosts = numOfPosts;
+        this.numOfPosts = getPosts().size();
         this.numOfFollowers = numOfFollowers;
         this.numOfFollowings = numOfFollowings;
     }
-    private static List<User> makeUsers(){
+
+    private static List<User> makeUsers() {
         List<User> users = new LinkedList<>();
-        users.add(new User("thanos", "thanos@gmail.com", "thanos", 10, 6,324));
-        users.add(new User("loki", "loki@gmail.com", "loki", 150, 57,444));
-        users.add(new User("thor", "thor@gmail.com", "thor", 123, 565,546));
-        users.add(new User("batman", "batman@gmail.com", "batman", 20, 3567,656));
-        users.add(new User("superman", "superman@gmail.com", "superman", 40, 9085,633));
+        users.add(new User("thanos", "thanos@gmail.com", "thanos", 6, 324));
+        users.add(new User("loki", "loki@gmail.com", "loki", 57, 444));
+        users.add(new User("thor", "thor@gmail.com", "thor", 565, 546));
+        users.add(new User("batman", "batman@gmail.com", "batman", 3567, 656));
+        users.add(new User("superman", "superman@gmail.com", "superman", 9085, 633));
         return users;
     }
-    public static List<User> getUsers(){
+
+    public static List<User> getUsers() {
         return users;
     }
-    public void addPost(Post newPost){
+
+    public void addPost(Post newPost) {
         getPosts().add(newPost);
         setPosts(getPosts());
+    }
+    public void updateNumOfPosts(){
+        this.numOfPosts = getPosts().size();
     }
 }
