e2f4f0ffc32dde6273a66117883b670cd408e8ba
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-03-12 03:24:41 +0600

    minor changes

--- src/main/java/kz/attractorschool/microgram/model/UserRepository.java
+++ src/main/java/kz/attractorschool/microgram/model/UserRepository.java
@@ -2,5 +2,7 @@ package kz.attractorschool.microgram.model;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User, String> {
 }
