6d8cd52f8e3471f8284280b44309f332dc6ae253
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-03-12 03:06:57 +0600

    2nd constructor added

--- src/main/java/kz/attractorschool/microgram/model/Comment.java
+++ src/main/java/kz/attractorschool/microgram/model/Comment.java
@@ -2,22 +2,33 @@ package kz.attractorschool.microgram.model;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
-@Document(collection = "Comments")
+@Document(collection = "comments")
 @Data
+//@CompoundIndex(def = "{'path': 1. 'userName': 1}")
 public class Comment {
 
     @Id
     private String id;
+    private String path = null;
     private String text;
     private LocalDateTime dateTime;
+    private String userName;
 
-    public Comment(String id, String text, LocalDateTime dateTime) {
-        this.id = id;
+    public Comment(String text, LocalDateTime dateTime, String userName) {
+        this.id = UUID.randomUUID().toString();
         this.text = text;
         this.dateTime = dateTime;
+        this.userName = userName;
+    }
+
+    public Comment(String path, String text, LocalDateTime dateTime, String userName) {
+        new Comment(text, dateTime, userName);
+        this.path = path;
     }
 }
