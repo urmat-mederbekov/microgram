e8677b85003172cba9678239aca8823cc48c98ea
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-03-16 05:56:40 +0600

    controller improved

--- src/main/java/kz/attractorschool/microgram/controller/UserController.java
+++ src/main/java/kz/attractorschool/microgram/controller/UserController.java
@@ -1,34 +1,93 @@
 package kz.attractorschool.microgram.controller;
 
-import kz.attractorschool.microgram.model.Post;
 import kz.attractorschool.microgram.model.User;
-import kz.attractorschool.microgram.model.UserRepository;
+import kz.attractorschool.microgram.repository.UserRepo;
+import org.hibernate.validator.constraints.EAN;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 public class UserController {
-    private final UserRepository userRepository;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    Sort sort = Sort.by(Sort.Order.asc("accountName"));
+
+    private final UserRepo userRepo;
+
+    public UserController(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @GetMapping("/")
+    public String homeGet() {
+        return "home";
+    }
+    @GetMapping("/home")
+    public String homeGet2() {
+        return "home";
+    }
+
+    @GetMapping("/hello")
+    public String hellGet() {
+        return "hello";
+    }
+    @GetMapping("/login")
+    public String loginGet() {
+        return "login";
+    }
+    @GetMapping("/users/name/{name}")
+    public String getUserByName(@PathVariable("name") String name, Model model) {
+        model.addAttribute("users", userRepo.findAllByAccountName(name, sort));
+        return "users";
+    }
+    @GetMapping("/users/otherpost/{name}")
+    public String getUserByNameExceptYou(@PathVariable("name") String name, Model model) {
+        model.addAttribute("users", userRepo.findAllByAccountNameNotContains(name, sort));
+        return "otherPosts";
+    }
+    @GetMapping("/users/explore/{name}")
+    public String getFollowingsPost(@PathVariable("name") String name,Model model) {
+        System.out.println("explore");
+//        List<User> x = User.getUsers()
+//                .stream()
+//                .filter(user -> user.isLoggedIn() != true)
+//                .collect(Collectors.toList());
+//        for(User user: User.getUsers()){
+//            for(User user1: user.getFollowers()){
+//                if (user1.equals(name)){
+//                    var x =
+//                }
+//            }
+//        }
+        model.addAttribute("users", userRepo.findAllByAccountName(name, sort));
+        return "otherPosts";
+    }
+    @GetMapping("/users/email/{email}")
+    public String getUserByEmail(@PathVariable("email") String email, Model model) {
+        Sort sort = Sort.by(Sort.Order.asc("email"));
+        model.addAttribute("users", userRepo.findAllByEmail(email, sort));
+        return "users";
+    }
+    @GetMapping("/users/exist/{email}")
+    public String checkUserExistenceByEmail(@PathVariable("email") String email, Model model) {
+        model.addAttribute("isExist", userRepo.existsUserByEmail(email));
+        return "existence";
     }
 
     @GetMapping("/users")
-    public String usersHandler(Model model) {
+    public String usersGet(Model model) {
         model.addAttribute("users", User.getUsers());
         return "users";
     }
     @GetMapping("users/user")
-    public String userHandler(@RequestParam("userId") String userId, Model model) {
-        User user = userRepository.findById(userId).orElse(User.EMPTY);
+    public String userGet(@RequestParam("userId") String userId, Model model) {
+        User user = userRepo.findById(userId).orElse(User.EMPTY);
 
-        User.getUsers().stream().forEach(u -> u.updateNumOfPosts());
-        Post.getPosts().stream().forEach(p -> p.updateNumOfLikes());
         model.addAttribute("user", user);
-        model.addAttribute("posts", user.getPosts());
-
         return "user";
     }
 }
