c30270b6b88f766080a54c3c888af77bdb64ee89
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-03-16 05:54:00 +0600

    slight changes

--- src/main/java/kz/attractorschool/microgram/utils/PreloadWithData.java
+++ src/main/java/kz/attractorschool/microgram/utils/PreloadWithData.java
@@ -1,18 +1,21 @@
 package kz.attractorschool.microgram.utils;
 
 import kz.attractorschool.microgram.model.*;
+import kz.attractorschool.microgram.repository.LikeRepo;
+import kz.attractorschool.microgram.repository.PostRepo;
+import kz.attractorschool.microgram.repository.SubscriptionRepo;
+import kz.attractorschool.microgram.repository.UserRepo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.time.LocalDateTime;
 import java.util.stream.Stream;
 
 @Configuration
 public class PreloadWithData {
 
     @Bean
-    CommandLineRunner initLikedDatabase(LikeRepository likeRep) {
+    CommandLineRunner initLikedDatabase(LikeRepo likeRep) {
         likeRep.deleteAll();
 
         Post.getPosts().get(2).addLike(Like.getLikes().get(0));
@@ -21,11 +24,12 @@ public class PreloadWithData {
                 .peek(likes -> likes.forEach(System.out::println))
                 .forEach(likeRep::saveAll);
     }
+
     @Bean
-    CommandLineRunner initPostDatabase(PostRepository postRep) {
+    CommandLineRunner initPostDatabase(PostRepo postRep) {
         postRep.deleteAll();
 
-        Post.getPosts().stream().forEach(post -> post.updateNumOfLikes());
+//        Post.getPosts().stream().forEach(Post::updateNumOfLikes);
 
         return (args) -> Stream.of((Post.getPosts()))
                 .peek(posts -> posts.forEach(System.out::println))
@@ -33,18 +37,40 @@ public class PreloadWithData {
     }
 
     @Bean
-    CommandLineRunner initUserDatabase(UserRepository userRep) {
+    CommandLineRunner initUserDatabase(UserRepo userRep) {
         userRep.deleteAll();
         Post post = Post.getPosts().get(2);
         User.getUsers().get(0).addPost(post);
         Post post1 = Post.getPosts().get(1);
         User.getUsers().get(0).addPost(post1);
 
-        User.getUsers().stream().forEach(user -> user.updateNumOfPosts());
+        User.getUsers().get(2).addFollowers(User.getUsers().get(4));
+//        User.getUsers().get(4).addFollowings(User.getUsers().get(2));
+//        User.subscribe(0,2);
+
+        User.getUsers().get(0).addFollowers(User.getUsers().get(1));
+//        User.getUsers().get(1).addFollowings(User.getUsers().get(0));
+//        User.subscribe(1,3);
+////
+        User.getUsers().get(3).addFollowers(User.getUsers().get(4));
+//        User.getUsers().get(4).addFollowings(User.getUsers().get(3));
+//        User.subscribe(3,0);
+        User.getUsers().get(3).addFollowers(User.getUsers().get(0));
+//        User.getUsers().get(0).addFollowings(User.getUsers().get(3));
+//        User.subscribe(0,4);
 
         return (args) -> Stream.of(User.getUsers())
                 .peek(users -> users.forEach(System.out::println))
                 .forEach(userRep::saveAll);
+    }
+
+    @Bean
+    CommandLineRunner initSubscriptionDatabase(SubscriptionRepo subscriptionRepo) {
+        subscriptionRepo.deleteAll();
+
+        return  (args) -> Stream.of(Subscription.getSubscriptions())
+                .peek(subscriptions -> subscriptions.forEach(System.out::println))
+                .forEach(subscriptionRepo::saveAll);
     }
 
     //    @Bean
@@ -54,14 +80,5 @@ public class PreloadWithData {
 //        return  (args) -> Stream.of(makeComments())
 //                .peek(System.out::println)
 //                .forEach(commentRep::save);
-//    }
-
-//    @Bean
-//    CommandLineRunner initSubscriptionDatabase(SubscriptionToUserRepository subscriptionToUserRep) {
-//        subscriptionToUserRep.deleteAll();
-//
-//        return  (args) -> Stream.of(makeSubscriptionToUser())
-//                .peek(System.out::println)
-//                .forEach(subscriptionToUserRep::save);
 //    }
 }
