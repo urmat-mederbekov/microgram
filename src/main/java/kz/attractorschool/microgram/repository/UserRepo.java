7d84639478dd3c0d8ed8d1baefb5dba9a1f1e9f0
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-03-19 01:10:37 +0600

    mehtods impoved

--- src/main/java/kz/attractorschool/microgram/repository/UserRepo.java
+++ src/main/java/kz/attractorschool/microgram/repository/UserRepo.java
@@ -1,16 +1,21 @@
 package kz.attractorschool.microgram.repository;
 
+import kz.attractorschool.microgram.model.Post;
 import kz.attractorschool.microgram.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface UserRepo extends CrudRepository<User, String> {
-    List<User> findAllByAccountName(String name, Sort sort);
-    List<User> findAllByEmail(String email, Sort sort);
-    boolean existsUserByEmail(String email);
-    List<User> findAllByAccountNameNotContains(String name, Sort sort);
-    List<User> findAllByFollowingsNotContains(List<User> users, Sort sort);
+public interface UserRepo extends PagingAndSortingRepository<User, String> {
 
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    Page<User> findAllByUsernameNotContains(Pageable pageable, String username);
+    void deleteByUsername(String username);
 }
