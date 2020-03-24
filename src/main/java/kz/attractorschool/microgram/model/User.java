4714e26089502c223a6737c5046e18c850da0d6b
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-03-24 19:41:21 +0600

    email is identificator now, id is deleted, class implements userdetails

--- src/main/java/kz/attractorschool/microgram/model/User.java
+++ src/main/java/kz/attractorschool/microgram/model/User.java
@@ -4,22 +4,24 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.UUID;
+import java.util.Collection;
+import java.util.List;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 @Document(collection = "users")
 @Data
-public class User {
+public class  User implements UserDetails {
     public static final User EMPTY = new User("No one", "none@gmail.com", "nothing");
 
-    @Id
-    private String id;
     @Indexed
     private String username;
-    @Indexed
+    @Id
     private String email;
     private String password;
     private int numOfPosts = 0;
@@ -27,10 +29,30 @@ public class User {
     private int numOfFollowings;
 
     public User(String username, String email, String password) {
-        this.id = UUID.randomUUID().toString();
         this.username = username;
         this.email = email;
         this.password = password;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("FULL"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
