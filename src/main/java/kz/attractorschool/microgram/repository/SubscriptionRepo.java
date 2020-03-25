644e6e9ffd2fc62a0b297509f8bc8bec716b942b
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-03-25 15:16:12 +0600

    method findAllByFollowerEmail added

--- src/main/java/kz/attractorschool/microgram/repository/SubscriptionRepo.java
+++ src/main/java/kz/attractorschool/microgram/repository/SubscriptionRepo.java
@@ -1,10 +1,13 @@
 package kz.attractorschool.microgram.repository;
 
 import kz.attractorschool.microgram.model.Subscription;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface SubscriptionRepo extends CrudRepository<Subscription, String> {
+public interface SubscriptionRepo extends PagingAndSortingRepository<Subscription, String> {
     int countByFollowerEmail(String email);
     int countByFollowingEmail(String email);
-    void deleteByFollowerUsername(String username);
+    Page<Subscription> findAllByFollowerEmail(Pageable pageable, String email);
 }
